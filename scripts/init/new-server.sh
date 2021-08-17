1ff6e7145be55dcb57245bcf126bd9d78bc31675
Tim White <tim@timwhite.io>
2021-08-17 05:06:01 +0200

    pico=working; homer=working; doku=working; kanboard=working

--- scripts/init/new-server.sh
+++ scripts/init/new-server.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ ! -f .env ]
-then
+FILE=./.env
+if [ -f $FILE ]; then
+  echo ".env file exists; exporting vars"
   export $(cat .env | xargs)
+else
+  echo "Nope .env file"
 fi
-
-
 
 get_picocms() {
 	# PicoCMS
 	curl -sSL https://getcomposer.org/installer | php
+	rm -rf ../../apps/picocms/html
 	git clone --depth 1 $PICO_COMPOSER_REPOSITORY ../../apps/picocms/html
 	php composer.phar --working-dir=../../apps/picocms/html/ install
 }
