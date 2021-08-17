72bea71e11595c5daa9a4b7452288bd8d445042d
Tim White <tim@timwhite.io>
2021-08-17 04:36:16 +0200

    don't let this get run until ready

--- scripts/backup/upload-script.sh
+++ scripts/backup/upload-script.sh
@@ -1,4 +1,8 @@
-sudo rclone sync /twiodata/backups b2:twio-data-backup
-sudo rclone cleanup b2:twio-data-backup
-sudo echo "Successfully uploaded: $(hostname)-$(date -Iseconds | cut -d '+' -f 1)" >> /var/log/backup.log
+#!/bin/bash
+
+#TODO: Convert to TWIO ecosystem
+
+#sudo rclone sync /twiodata/backups b2:twio-data-backup
+#sudo rclone cleanup b2:twio-data-backup
+#sudo echo "Successfully uploaded: $(hostname)-$(date -Iseconds | cut -d '+' -f 1)" >> /var/log/backup.log
 
