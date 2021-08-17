75d5c73af2ee9c083e0123a09f044bb710e27c90
Tim White <tim@timwhite.io>
2021-08-17 04:53:53 +0200

    tweaks

--- scripts/init/new-server.sh
+++ scripts/init/new-server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-get_env_vars() {
-	if [ ! -f ../.env ]
-	then
-	  export $(cat ../.env | xargs)
-	fi
-}
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+
 
 get_picocms() {
 	# PicoCMS
 	curl -sSL https://getcomposer.org/installer | php
-	git clone --depth 1 $PICO_COMPOSER_REPOSITORY ../apps/picocms/html
-	php composer.phar --working-dir=../apps/picocms/html/ install
+	git clone --depth 1 $PICO_COMPOSER_REPOSITORY ../../apps/picocms/html
+	php composer.phar --working-dir=../../apps/picocms/html/ install
 }
 
 #get_data_repo() {
@@ -19,9 +19,10 @@ get_picocms() {
 #	if [[ -v DATA_REPOSITORY ]]; then git clone $DATA_REPOSITORY ./data; else echo "Skipping data. No repository set"; fi
 #}
 
-#get_env_vars
-#get_picocms
+get_picocms
 #get_data_repo
+
+
 
 docker network create --driver bridge net || true
 docker-compose up -d
