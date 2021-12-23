0b50559ffef9677fdbeb1a1444478006ce1f6489
twwhite <tim@timwhite.io>
2021-12-23 22:31:48 +0100

    add config.php with placeholder info

---
+++ apps/kanboard/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// We choose to use MySQL instead of Sqlite
+define('DB_DRIVER', 'mysql');
+
+// MySQL parameters
+define('DB_USERNAME', 'kanboard');
+define('DB_PASSWORD', 'temppass123');
+define('DB_HOSTNAME', 'db');
+define('DB_NAME', 'kanboard');
