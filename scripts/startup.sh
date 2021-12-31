f2eac6391ccf5bab2368d4347c99d22ce8ac784f
twwhite <tim@timwhite.io>
2021-12-31 22:50:53 +0100

    progress - commit messages are fun

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+
 cd /twio/scripts/
-docker-compose down && docker-compose up -d
+
+import_env(){
+  # Import .env vars -> Carries over to docker-compose.yml
+  FILE=./.env
+
+  if [ -f $FILE ]; then
+    echo "Loading variables from $FILE"
+    set -o allexport
+    source $FILE
+    set +o allexport
+  else
+    echo "Please setup a .env file according to the README.md"
+    exit 1
+  fi
+
+  echo "Variables loaded!"
+}
+
+
+launch(){
+  docker-compose down && docker-compose up -d
+}
+
+import_env
+launch
